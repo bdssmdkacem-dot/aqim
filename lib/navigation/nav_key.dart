4a9415344b3d1fb5fea8eb40cc793149facaca39
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-08-12 22:18:26 +0100

    Delete lib directory

--- lib/navigation/nav_key.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// مفتاح تنقّل عام يُستعمل داخل MaterialApp، ويسمح لخدمة الإشعارات
-/// بفتح شاشة معيّنة عند الضغط على إشعار حتى لو كان التطبيق فـ الخلفية.
-final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();

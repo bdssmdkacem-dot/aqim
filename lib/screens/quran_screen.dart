22113326a6d0e6199bbc4370bca3d38669118197
ressourcesapmm-lgtm <ressourcesapmm@gmail.com>
2026-08-27 20:17:03 +0100

    fix: restore Quran notification riwaya

--- lib/screens/quran_screen.dart
+++ lib/screens/quran_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'mushaf_quran_screen.dart';
 
 /// Backwards-compatible entry point used by AQIM navigation.
-/// The Quran screen is now the real offline page-based Mushaf viewer.
-class QuranScreen extends StatelessWidget {
+/// Restores the last Mushaf riwaya when opened from a Quran notification.
+class QuranScreen extends StatefulWidget {
   final int? initialPage;
   const QuranScreen({super.key, this.initialPage});
 
   @override
-  Widget build(BuildContext context) => MushafQuranScreen(initialPage: initialPage);
+  State<QuranScreen> createState() => _QuranScreenState();
+}
+
+class _QuranScreenState extends State<QuranScreen> {
+  MushafRiwaya _riwaya = MushafRiwaya.hafs;
+
+  @override
+  void initState() {
+    super.initState();
+    _restoreRiwaya();
+  }
+
+  Future<void> _restoreRiwaya() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getString('quran_last_riwaya');
+    if (!mounted) return;
+    setState(() {
+      _riwaya = saved == MushafRiwaya.warsh.name
+          ? MushafRiwaya.warsh
+          : MushafRiwaya.hafs;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => MushafQuranScreen(
+        initialPage: widget.initialPage,
+        initialRiwaya: _riwaya,
+      );
 }

30940289d903ff0914f16837691fcc51616209fd
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-26 05:20:29 +0100

    feat(quran-audio): add screen skeleton

--- lib/screens/quran_audio_screen.dart
+++ lib/screens/quran_audio_screen.dart
@@ -1,1 +1,2 @@
-// placeholder
+import 'package:flutter/material.dart';
+class QuranAudioScreen extends StatelessWidget { const QuranAudioScreen({super.key}); @override Widget build(BuildContext context)=>const Scaffold(body:Center(child:Text('Quran audio'))); }
